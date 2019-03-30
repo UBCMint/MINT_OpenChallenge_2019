ce99df04321c1ed9558b9b55ebdae2883c7df111
ellehcim8174 <michelle.mak.8174@gmail.com>
2019-03-30 10:44:58 -0700

    New branch commit

---
+++ app/src/main/java/ca/ubc/best/mint/museandroidapp/InitialActivity.java
@@ -0,0 +1,200 @@
+package ca.ubc.best.mint.museandroidapp;
+
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.bluetooth.BluetoothAdapter;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.databinding.DataBindingUtil;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.choosemuse.libmuse.Muse;
+import com.choosemuse.libmuse.MuseManagerAndroid;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import ca.ubc.best.mint.museandroidapp.databinding.ActivityInitialBinding;
+import ca.ubc.best.mint.museandroidapp.vm.InitialViewModel;
+import eeg.useit.today.eegtoolkit.vm.MuseListViewModel;
+
+public class InitialActivity extends AppCompatActivity {
+  private static final int REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS = 124;
+  private static final int REQUEST_CODE_ENABLE_BT = 1;
+  private static final int SCAN_LENGTH_SEC = 5;
+
+  /** ViewModel for this activity. */
+  public final InitialViewModel viewModel = new InitialViewModel();
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    MuseManagerAndroid.getInstance().setContext(InitialActivity.this);
+
+    ActivityInitialBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_initial);
+    binding.setActivity(this);
+    binding.setViewModel(viewModel);
+    getSupportActionBar().setTitle(R.string.app_name);
+
+    maybeAskPermissions();
+  }
+
+  /** Handles the scan button being clicked: starts the scan for muse devices. */
+  public void handleScanClicked() {
+    BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    if (mBluetoothAdapter == null) {
+      Toast.makeText(this, "Bluetooth is not supported on this device :(", Toast.LENGTH_LONG).show();
+    } else {
+      // Only scan if bluetooth is enabled, otherwise turn on bluetooth first...
+      if (mBluetoothAdapter.isEnabled()) {
+        scanForDevices();
+      } else {
+        enableBluetooth();
+      }
+    }
+  }
+
+  /** Handles skip button clicked, debug shortcut to go to results screen with fake data. */
+  public void viewHistoryClicked() {
+    Intent intent = new Intent(InitialActivity.this, ResultListActivity.class);
+    startActivity(intent);
+  }
+
+
+  private void scanForDevices() {
+    viewModel.scanForDevice(SCAN_LENGTH_SEC, new MuseListViewModel.MuseListListener() {
+      @Override
+      public void onScanForDevicesFinished() { /* not called */ }
+
+      @Override
+      public void onDeviceSelected(Muse muse) {
+        if (muse == null) {
+          Toast.makeText(InitialActivity.this, "No devices found :(", Toast.LENGTH_LONG).show();
+        } else {
+          Log.i("MINT", "Device found: " + muse.getName());
+          Intent intent = new Intent(InitialActivity.this, FlankerActivity.class);
+          intent.putExtra("MAC", muse.getMacAddress());
+          startActivity(intent);
+        }
+      }
+    });
+  }
+
+  // HACK - permissions:
+  //
+  // Permissions code below. See
+  // https://stackoverflow.com/questions/32708374/bluetooth-le-scanfilters-dont-work-on-android-m
+  //
+
+  @Override
+  public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+    switch (requestCode) {
+      case REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS:
+        Map<String, Integer> perms = new HashMap<>();
+        perms.put(Manifest.permission.ACCESS_FINE_LOCATION, PackageManager.PERMISSION_GRANTED);
+        for (int i = 0; i < permissions.length; i++) {
+          perms.put(permissions[i], grantResults[i]);
+        }
+
+        if (perms.get(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+          Toast.makeText(
+              InitialActivity.this,
+              "All Permission GRANTED !! Thank You :)",
+              Toast.LENGTH_SHORT).show();
+        } else {
+          Toast.makeText(
+              InitialActivity.this,
+              "One or More Permissions are DENIED Exiting App :(",
+              Toast.LENGTH_SHORT).show();
+          finish();
+        }
+        break;
+      default:
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+  }
+
+  @TargetApi(Build.VERSION_CODES.M)
+  private void maybeAskPermissions() {
+    List<String> permissionsNeeded = new ArrayList<>();
+    final List<String> permissionsList = new ArrayList<>();
+    if (!addPermission(permissionsList, Manifest.permission.ACCESS_FINE_LOCATION)) {
+      permissionsNeeded.add("Show Location");
+    }
+    if (!addPermission(permissionsList, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+      permissionsNeeded.add("Write to file");
+    }
+    if (!addPermission(permissionsList, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+      permissionsNeeded.add("Read file");
+    }
+
+    if (permissionsList.size() > 0) {
+      if (permissionsNeeded.size() > 0) {
+        String message = "App need access to " + permissionsNeeded.get(0);
+        for (int i = 1; i < permissionsNeeded.size(); i++) {
+          message = message + ", " + permissionsNeeded.get(i);
+        }
+        showMessageOKCancel(message, new DialogInterface.OnClickListener() {
+          @Override
+          public void onClick(DialogInterface dialog, int which) {
+            requestPermissions(
+                permissionsList.toArray(new String[permissionsList.size()]),
+                REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS
+            );
+          }
+        });
+        return;
+      }
+      requestPermissions(
+          permissionsList.toArray(new String[permissionsList.size()]),
+          REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS
+      );
+      return;
+    }
+  }
+
+  private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
+    new AlertDialog.Builder(InitialActivity.this)
+        .setMessage(message)
+        .setPositiveButton("OK", okListener)
+        .setNegativeButton("Cancel", null)
+        .create()
+        .show();
+  }
+
+  @TargetApi(Build.VERSION_CODES.M)
+  private boolean addPermission(List<String> permissionsList, String permission) {
+    if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+      permissionsList.add(permission);
+      // Check for Rationale Option
+      if (!shouldShowRequestPermissionRationale(permission)) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  /** Fire an intent which asks the user to turn on bluetooth. */
+  private void enableBluetooth() {
+    Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+    startActivityForResult(enableBtIntent, REQUEST_CODE_ENABLE_BT);
+  }
+
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+    // Enable-bluetooth intent has come back accepted, bluetooth is on, so start the scan.
+    if (requestCode == REQUEST_CODE_ENABLE_BT && resultCode == RESULT_OK) {
+      scanForDevices();
+    }
+  }
+}
---
+++ app/src/main/java/ca/ubc/best/mint/museandroidapp/vm/StimulusCue.java
@@ -0,0 +1,260 @@
+package ca.ubc.best.mint.museandroidapp.vm;
+
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by Carolfa on 2018-02-23.
+ */
+
+public class StimulusCue {
+    public FlankerStimulus stimulus;
+    public FlankerCue cue;
+    public StimulusCue (FlankerStimulus stimulus, FlankerCue cue) {
+        this.stimulus = stimulus;
+        this.cue = cue;
+    }
+
+    public static List<StimulusCue> createNeutralStimuliCueList(List<FlankerStimulus> neutralStimuliList) {
+        /** Neutral Stimuli are preceded by:
+            76.4% NULL cues
+            23.6% RP cues, out of all RP cues 84% are the correct cue, 16% are the wrong cue.
+            For the correct RP cues, 27.7% are followed by a catch stimuli "+++++",
+               users are instructed to tap on the side indicated by the RP cue
+         **/
+        List<StimulusCue> neutralStimulusCueSet = new ArrayList<>();
+
+        int arraySize = neutralStimuliList.size();
+        int numNullCues = Math.round(0.764f * arraySize);
+        int numRPCues = arraySize - numNullCues; // All cues are null or RP
+        int numWrongRPCues = Math.round(0.16f * numRPCues);
+        int numRightRPCues = numRPCues - numWrongRPCues;
+        int numCorNCatRPCues = Math.round(0.723f * numRightRPCues);
+        int numCorCatchRPCues = numRightRPCues - numCorNCatRPCues;
+        assert numNullCues + numWrongRPCues + numCorNCatRPCues + numCorCatchRPCues == arraySize;
+
+        Log.d("arraySize", String.valueOf(arraySize));
+        Log.d("numNullCues", String.valueOf(numNullCues));
+        Log.d("numRPCues", String.valueOf(numRPCues));
+        Log.d("numWrongRPCues", String.valueOf(numWrongRPCues));
+        Log.d("numRightRPCues", String.valueOf(numRightRPCues));
+        Log.d("numCorNCatRPCues", String.valueOf(numCorNCatRPCues));
+        Log.d("numCorCatchRPCues", String.valueOf(numCorCatchRPCues));
+
+
+        /**Add null cues**/
+        int index = 0;
+
+        //add null stimulus
+        for(int i = 0; i < numNullCues; i++) {
+            neutralStimulusCueSet.add(new StimulusCue(neutralStimuliList.get(index), FlankerCue.NULL));
+            index ++;
+        }
+
+        //add wrong rp cues
+        for(int i=0;i<numWrongRPCues; i++) {
+            if(neutralStimuliList.get(index) == FlankerStimulus.LEFTNEUTRAL){
+                // add Right RP cue if the stimulus is Left Neutral
+                neutralStimulusCueSet.add(new StimulusCue(neutralStimuliList.get(index), FlankerCue.RRP));
+            }
+            else {
+                // else add Left RP cue
+                neutralStimulusCueSet.add(new StimulusCue(neutralStimuliList.get(index), FlankerCue.LRP));
+            }
+            index ++;
+        }
+
+        //add CORRECT non-catch rp cues
+        for(int i=0; i < numCorNCatRPCues; i++) {
+            if(neutralStimuliList.get(index) == FlankerStimulus.LEFTNEUTRAL){
+                // add Left RP cue if the stimulus is Left Neutral
+                neutralStimulusCueSet.add(new StimulusCue(neutralStimuliList.get(index), FlankerCue.LRP));
+            }
+            else {
+                // else add Right RP cue
+                neutralStimulusCueSet.add(new StimulusCue(neutralStimuliList.get(index), FlankerCue.RRP));
+            }
+            index ++;
+        }
+
+        //add CORRECT Catch rp cues
+
+        //half has LRP cues
+        for(int i=0; i<numCorCatchRPCues/2; i++) {
+            neutralStimulusCueSet.add(new StimulusCue(FlankerStimulus.CATCH, FlankerCue.LRP));
+            index ++;
+        }
+        //half has RRP cues
+        for(int i=numCorCatchRPCues/2;i<numCorCatchRPCues; i++) {
+            neutralStimulusCueSet.add(new StimulusCue(FlankerStimulus.CATCH, FlankerCue.RRP));
+            index ++;
+        }
+
+        Collections.shuffle(neutralStimulusCueSet); //randomize the array
+        Log.d("MINT", "Created Neutral Stimulus Cue Set: " + neutralStimulusCueSet.toString());
+        assert neutralStimulusCueSet.size() == neutralStimuliList.size();
+        return neutralStimulusCueSet;
+
+    }
+
+
+    public static List<StimulusCue> createCongStimuliCueList(List<FlankerStimulus> congStimuliList) {
+        /** Congruent Stimuli are preceded by:
+         76.4% NULL cues
+         23.6% RP cues, out of all RP cues 84% are the correct cue, 16% are the wrong cue.
+         For the correct RP cues, 27.7% are followed by a catch stimuli "+++++",
+            users are instructed to tap on the side indicated by the RP cue
+         **/
+        List<StimulusCue> congStimulusCueSet = new ArrayList<>();
+
+        int arraySize = congStimuliList.size();
+        int numNullCues = Math.round(0.764f * arraySize);
+        int numRPCues = arraySize - numNullCues; // All cues are null or RP
+        int numWrongRPCues = Math.round(0.16f * numRPCues);
+        int numRightRPCues = numRPCues - numWrongRPCues;
+        int numCorNCatRPCues = Math.round(0.723f * numRightRPCues);
+        int numCorCatchRPCues = numRightRPCues - numCorNCatRPCues;
+        assert numNullCues + numWrongRPCues + numCorNCatRPCues + numCorCatchRPCues == arraySize;
+
+
+        /**Add null cues**/
+        int index = 0;
+
+        //half left neutral stimulus
+        for(int i=0;i<numNullCues;i++) {
+            congStimulusCueSet.add(new StimulusCue(congStimuliList.get(index), FlankerCue.NULL));
+            index ++;
+        }
+
+        //add wrong rp cues
+        for(int i=0; i<numWrongRPCues; i++) {
+            if(congStimuliList.get(index) == FlankerStimulus.LEFTCONGRUENT){
+                // add Right RP cue if the stimulus is Left Congruent
+                congStimulusCueSet.add(new StimulusCue(congStimuliList.get(index), FlankerCue.RRP));
+            }
+            else {
+                // else add Left RP cue
+                congStimulusCueSet.add(new StimulusCue(congStimuliList.get(index), FlankerCue.LRP));
+            }
+            index ++;
+        }
+
+        //add CORRECT non-catch rp cues
+        for(int i=0; i<numCorNCatRPCues; i++) {
+            if(congStimuliList.get(index) == FlankerStimulus.LEFTCONGRUENT){
+                // add Left RP cue if the stimulus is Left Congruent
+                congStimulusCueSet.add(new StimulusCue(congStimuliList.get(index), FlankerCue.LRP));
+            }
+            else {
+                // else add Right RP cue
+                congStimulusCueSet.add(new StimulusCue(congStimuliList.get(index), FlankerCue.RRP));
+            }
+            index++;
+        }
+
+        //add CORRECT Catch rp cues
+
+        //half has LRP cues
+        for(int i=0; i<numCorCatchRPCues/2; i++) {
+            congStimulusCueSet.add(new StimulusCue(FlankerStimulus.CATCH, FlankerCue.LRP));
+            index ++;
+        }
+        //half has RRP cues
+        for(int i=numCorCatchRPCues/2; i<numCorCatchRPCues; i++) {
+            congStimulusCueSet.add(new StimulusCue(FlankerStimulus.CATCH, FlankerCue.RRP));
+            index ++;
+        }
+
+        Collections.shuffle(congStimulusCueSet); //randomize the array
+        Log.d("MINT", "Created Congruent Stimulus Cue Set: " + congStimulusCueSet.toString());
+        assert congStimulusCueSet.size() == congStimuliList.size();
+        return congStimulusCueSet;
+    }
+
+
+    public static List<StimulusCue> createIncongStimuliCueList(List<FlankerStimulus> incongStimuliList) {
+        /** Incongruent Stimuli are preceded by:
+         47.5% NULL cues
+         26.25% WARN cues
+         26.25% RP cues, out of all RP cues 84% are the correct cue, 16% are the wrong cue.
+         For the correct RP cues, 27.7% are followed by a catch stimuli "+++++", users are instructed to tap on the side indicated by the RP cue
+
+         **/
+        List<StimulusCue> incongStimulusCueSet = new ArrayList<>();
+
+        int arraySize = incongStimuliList.size();
+        int numNullCues = Math.round(0.475f * arraySize);
+        int numWarnCues = Math.round(0.2625f * arraySize);
+        int numRPCues = arraySize - numNullCues - numWarnCues;
+        int numWrongRPCues = Math.round(0.16f * numRPCues);
+        int numRightRPCues = numRPCues - numWrongRPCues;
+        int numCorNCatRPCues = Math.round(0.723f * numRightRPCues);
+        int numCorCatchRPCues = numRightRPCues - numCorNCatRPCues;
+        assert numNullCues + numWarnCues + numWrongRPCues + numCorNCatRPCues + numCorCatchRPCues == arraySize;
+
+        int index = 0;
+
+        /**Add null cues**/
+        for(int i=0; i<numNullCues; i++) {
+            incongStimulusCueSet.add(new StimulusCue(incongStimuliList.get(index), FlankerCue.NULL));
+            index ++;
+        }
+
+        /** Add Warning cues **/
+        for(int i=0; i<numWarnCues; i++) {
+            incongStimulusCueSet.add(new StimulusCue(incongStimuliList.get(index), FlankerCue.WARN));
+            index++;
+        }
+
+
+        /** Add RP cues **/
+
+        //add wrong rp cues
+        for(int i=0;i<numWrongRPCues; i++) {
+            if(incongStimuliList.get(index) == FlankerStimulus.LEFTINCONGRUENT){
+                // add Right RP cue if the stimulus is Left Incongruent
+                incongStimulusCueSet.add(new StimulusCue(incongStimuliList.get(index), FlankerCue.RRP));
+            }
+            else {
+                // else add Left RP cue
+                incongStimulusCueSet.add(new StimulusCue(incongStimuliList.get(index), FlankerCue.LRP));
+            }
+            index ++;
+        }
+
+        //add CORRECT non-catch rp cues
+        for(int i=0;i<numCorNCatRPCues; i++) {
+            if(incongStimuliList.get(index) == FlankerStimulus.LEFTINCONGRUENT){
+                // add Left RP cue if the stimulus is Left Incongruent
+                incongStimulusCueSet.add(new StimulusCue(incongStimuliList.get(index), FlankerCue.LRP));
+            }
+            else {
+                // else add Right RP cue
+                incongStimulusCueSet.add(new StimulusCue(incongStimuliList.get(index), FlankerCue.RRP));
+            }
+            index ++;
+        }
+
+        //add CORRECT Catch rp cues
+
+        //half has LRP cues
+        for(int i=0; i<numCorCatchRPCues/2; i++) {
+            incongStimulusCueSet.add(new StimulusCue(FlankerStimulus.CATCH, FlankerCue.LRP));
+            index ++;
+        }
+        //half has RRP cues
+        //TODO or use index to fill up the rest of the incongStimliList to the correct cue
+        for(int i=numCorCatchRPCues/2;i<numCorCatchRPCues; i++) {
+            incongStimulusCueSet.add(new StimulusCue(FlankerStimulus.CATCH, FlankerCue.RRP));
+            index ++;
+        }
+
+        Collections.shuffle(incongStimulusCueSet); //randomize the array
+        Log.d("MINT", "Created Incongruent Stimulus Cue Set: " + incongStimulusCueSet.toString());
+        assert incongStimulusCueSet.size() == incongStimuliList.size();
+        return incongStimulusCueSet;
+    }
+}
