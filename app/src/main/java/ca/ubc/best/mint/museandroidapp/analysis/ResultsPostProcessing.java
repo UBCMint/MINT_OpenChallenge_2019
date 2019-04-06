fd17d82cd99b2ec3439130f8475b5d03d6e8225d
Bowen Jiang <bowengary@hotmail.com>
2019-04-06 14:28:31 -0700

    Preliminary code to get alpha surpression

--- app/src/main/java/ca/ubc/best/mint/museandroidapp/analysis/ResultsPostProcessing.java
+++ app/src/main/java/ca/ubc/best/mint/museandroidapp/analysis/ResultsPostProcessing.java
@@ -32,6 +32,8 @@ public class ResultsPostProcessing {
   private static final int BETA_SUPPRESSION_START_SAMPLES = msToSamples(800);
   private static final int BETA_SUPPRESSION_END_SAMPLES = msToSamples(BETA_END_MS);
 
+  private static final int threshold = 0;
+
   private ResultsPostProcessing() { /* Don't create. */ }
 
   /**
@@ -86,19 +88,74 @@ public class ResultsPostProcessing {
 
   /** @return Alpha suppression, calculated as the average drop in the desired time frame. */
   private static double calcAlphaSuppression(List<Map<String, TimeSeriesSnapshot<Double>>> alphas) {
-    double sum = 0.0;
-    int count = 0;
-    for (Map<String, TimeSeriesSnapshot<Double>> epochs : alphas) {
-      for (TimeSeriesSnapshot<Double> snapshot : epochs.values()) {
-        sum += averageInRange(
-            snapshot.values,
-            ALPHA_SUPPRESSION_START_SAMPLES,
-            ALPHA_SUPPRESSION_END_SAMPLES
-        ) * -1.0; // Negate, as higher suppression = lower values.
-        count++;
+    Double[] alphaSurpression = parseAlpha(alphas);
+    return alphaSurpression[0];
+  }
+
+  // Saves the Alpha data and the corresponding time stamp into a text file
+  private static Double[] parseAlpha(List<Map<String, TimeSeriesSnapshot<Double>>> alphas) {
+    ArrayList<Double> alphaData = new ArrayList<>();
+    //long[] timeStamps;
+    for(int i = 0; i < alphas.size(); i++) {
+      for (Map.Entry<String, TimeSeriesSnapshot<Double>> test : alphas.get(i).entrySet()) {
+        for(int j = 0; j < test.getValue().values.length; j++) {
+          alphaData.add(test.getValue().values[j]);
+        }
+        //System.arraycopy(test.getValue().values, 0, outputData, 0, test.getValue().values.length);
+        //timeStamps = new long[test.getValue().timestamps.length];
+        //System.arraycopy(test.getValue().timestamps, 0, timeStamps, 0, test.getValue().timestamps.length);
       }
     }
-    return sum / count;
+    Double[] alphaArray = new Double[alphaData.size()];
+    for(int i = 0; i < alphaArray.length; i++) {
+      alphaArray[i] = alphaData.get(i);
+    }
+    return alphaThreshold(alphaArray);
+
+  }
+
+  private static Double[] alphaThreshold(Double[] alphaData) {
+    Double[] beforeAlpha = new Double[alphaData.length];
+    Double[] afterAlpha = new Double[alphaData.length];
+    Double[] filteredDips = new Double[alphaData.length];
+    Double[] derivativeAlpha = getDerivative(alphaData);
+    Double[] averageVoltages = new Double[2];
+    Map<Integer, Double> alphaDips = new HashMap<>();
+
+    double sumBefore = 0;
+    double sumAfter = 0;
+    int counter = 0;
+
+    for(int i = 0; i < derivativeAlpha.length; i++) {
+      if(derivativeAlpha[i] < threshold) {
+        alphaDips.put(i, alphaData[i]);
+      }
+    }
+
+    for (Map.Entry<Integer, Double> test : alphaDips.entrySet()) {
+      alphaData[test.getKey() - 1] = beforeAlpha[counter];
+      alphaData[test.getKey() + 1] = afterAlpha[counter];
+      filteredDips[counter] = test.getValue();
+      counter++;
+    }
+
+    for(int j = 0; j < counter; j++) {
+      sumBefore += Math.abs(filteredDips[j] - beforeAlpha[j]);
+    }
+
+    for(int k = 0; k < counter; k++) {
+      sumAfter += Math.abs(filteredDips[k] - afterAlpha[k]);
+    }
+
+    averageVoltages[0] = sumBefore/counter;
+    averageVoltages[1] = sumAfter/counter;
+
+    return averageVoltages;
+  }
+
+  private static Double[] getDerivative(Double[] alphaData) {
+    Double[] output = {0.0};
+    return output;
   }
 
   /** @return Beta suppression, calculated as the average drop in the desired time frame. */
